636de0f682250c12a88e517cab065ff3e6cffb48
Alexander Shuvalov <ashuv_vyksa@mail.ru>
2020-02-17 16:15:25 +0300

    add redux-form

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import address from './address';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
   user: auth,
-  addressList: address
+  addressList: address,
+  form: formReducer
 });
